f1ef33731944b39638cb5b0bac2555cbf621c0fb
Gworld06 <113289400+Gworld06@users.noreply.github.com>
2026-09-19 18:21:26 +0530

    Create build.gradle.kts

---
+++ NxshaProvider/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    description = "Nxsha movie provider"
+    authors = listOf("YourName")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    iconUrl = "https://web.nxsha.app/favicon.ico"
+    language = "en"
+}
